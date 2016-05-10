d67f92e00c577926619c380073440e6bb9ec47c7
Florian Baumann <flo@noqqe.de>
2016-05-10 10:31:13 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 BLOGDIR=~/Code/entbehrlich.es/
 
 cd $BLOGDIR || exit 1
-rm -rf /public/
+rm -rf public/
 /home/noqqe/.go//bin/hugo
 /usr/local/bin/rsync -avi --delete public/ aax:/var/www/htdocs/entbehrlich.es/
 cd - &>/dev/null
